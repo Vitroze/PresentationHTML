842917c1219eec59bdba2662cee98c4265300c0d
Vitroze Gaming <azouaouiaziz4@gmail.com>
2023-10-08 12:36:16 +0200

    Code update. Finished home page

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace PresentationHTML
+﻿using System.Windows.Forms;
+
+namespace PresentationHTML
 {
     partial class Form1
     {
@@ -20,20 +22,72 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+    #region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
+    /// <summary>
+    ///  Required method for Designer support - do not modify
+    ///  the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_close = new System.Windows.Forms.Button();
+            this.button_next = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_close
+            // 
+            this.button_close.BackColor = System.Drawing.Color.Transparent;
+            this.button_close.BackgroundImage = global::PresentationHTML.Properties.Resources.button_close;
+            this.button_close.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button_close.FlatAppearance.BorderSize = 0;
+            this.button_close.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button_close.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_close.Location = new System.Drawing.Point(745, 12);
+            this.button_close.Name = "button_close";
+            this.button_close.Size = new System.Drawing.Size(60, 55);
+            this.button_close.TabIndex = 1;
+            this.button_close.UseVisualStyleBackColor = false;
+            this.button_close.Click += new System.EventHandler(this.button_close_Click);
+            // 
+            // button_next
+            // 
+            this.button_next.BackColor = System.Drawing.Color.Transparent;
+            this.button_next.BackgroundImage = global::PresentationHTML.Properties.Resources.button_next;
+            this.button_next.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button_next.FlatAppearance.BorderSize = 0;
+            this.button_next.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button_next.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button_next.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_next.Location = new System.Drawing.Point(585, 431);
+            this.button_next.Name = "button_next";
+            this.button_next.Size = new System.Drawing.Size(239, 57);
+            this.button_next.TabIndex = 2;
+            this.button_next.UseVisualStyleBackColor = false;
+            this.button_next.Click += new System.EventHandler(this.button_next_Click);
+            this.button_next.MouseLeave += new System.EventHandler(this.button_next_MouseExitHover);
+            this.button_next.MouseHover += new System.EventHandler(this.button_next_MouseHover);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::PresentationHTML.Properties.Resources.background_image;
+            this.ClientSize = new System.Drawing.Size(836, 509);
+            this.Controls.Add(this.button_next);
+            this.Controls.Add(this.button_close);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "HTML TP - Etape 1";
+            this.TransparencyKey = System.Drawing.Color.White;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private Button button_close;
+        private Button button_next;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,41 @@ namespace PresentationHTML
         public Form1()
         {
             InitializeComponent();
+            MessageBox.Show(this, "Ceci est une application expérimentale. Si il y a des problèmes merci de me le signaler", "Information - Developpement");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_next_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Test");
+        }
+
+        private void button_next_MouseHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Hand;
+            this.button_next.BackColor = System.Drawing.Color.Transparent;
+            this.button_next.BackgroundImage = Properties.Resources.button_next_hover;
+        }
+
+        private void button_next_MouseExitHover(object sender, EventArgs e)
+        {
+            this.Cursor = Cursors.Default;
+            this.button_next.BackColor = System.Drawing.Color.Transparent;
+            this.button_next.BackgroundImage = Properties.Resources.button_next;
+        }
+        private void button_close_Click(object sender, EventArgs e)
+        {
+            // Close the application
+            Application.Exit();
+        }
+
+        private void button_next_Click_1(object sender, EventArgs e)
+        {
+
         }
     }
 }
